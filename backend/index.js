e2840e203a57883cad4fb152c1e68e7e1f41ec1b
NaveenSandaru <naveensandaru2@gmail.com>
2025-05-01 09:19:00 +0530

    main

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,19 @@
+//import libraries
 import express, {json} from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-import {dirname, join} from 'path';
-import { fileURLToPath } from 'url';
 
-import counsellorRouter from './routes/counsellor-routes.js';
-import adminRouter from './routes/admin-routes.js';
-import superAdminRouter from './routes/super-admin-routes.js';
-import leadRouter from './routes/lead-routes.js';
-import commentRouter from './routes/comment-route.js';
-import deadRouter from './routes/dead-routes.js';
-import authRouter from './auth-routes/auth-route.js';
+//import endpoint routers
+import authRouter from './auth-routes/user-auth-route.js';
+import checkoutHistoryRouter from './routes/checkout-history-routes.js';
+import emailVerificationRouter from './routes/email-verification-routes.js';
+import paymentRouter from './routes/payment-routes.js';
+import shippingRouter from './routes/shipping-routes.js';
+import shoppingCartRouter from './routes/shopping-cart-routes.js';
+import userRouter from './routes/user-routes.js';
 
 dotenv.config();
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -24,14 +22,15 @@ const corsOptions = {credentials: true, origin: 'http://localhost:3000'};
 app.use(cors(corsOptions));
 app.use(json());
 app.use(cookieParser());
-app.use('/',express.static(join(__dirname, 'public')));
 
-app.use('/counsellors',counsellorRouter);
-app.use('/admins',adminRouter);
-app.use('/superadmins',superAdminRouter);
-app.use('/leads',leadRouter);
-app.use('/dead',deadRouter);
+//configure routers to redirect to endpoints
 app.use('/auth', authRouter);
-app.use('/comments', commentRouter);
+app.use('/checkout-history', checkoutHistoryRouter);
+app.use('/email-verification', emailVerificationRouter);
+app.use('/payments', paymentRouter);
+app.use('/shipping-info', shippingRouter);
+app.use('/shopping-cart', shoppingCartRouter);
+app.use('/user', userRouter);
+
 
 app.listen(PORT, ()=>console.log(`Server listening on ${PORT}`));
